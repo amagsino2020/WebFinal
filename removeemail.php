c95919ac9266a32bf227ea6808b435c0a46db2a4
Allura Magsino <70418023+amagsino2020@users.noreply.github.com>
2020-11-19 18:02:46 -0600

    Remove email PHP

---
+++ removeemail.php
@@ -0,0 +1,154 @@
+<html>
+<title>Remove Email</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.w3-sidebar a {font-family: "Roboto", sans-serif}
+body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
+</style>
+<body class="w3-content" style="max-width:1200px">
+
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
+  <div class="w3-container w3-display-container w3-padding-16">
+    <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
+    <h3 class="w3-wide" style = "text-align: center"><b>SNEAKER HEAD</b></h3>
+  </div>
+  <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
+    <a href="#" class="w3-bar-item w3-button">Handbags</a>
+    <a href="#" class="w3-bar-item w3-button">Streetwear</a>
+    <a onclick="myAccFunc()" href="javascript:void(0)" class="w3-button w3-block w3-white w3-left-align" id="myBtn">
+      Popular Brands <i class="fa fa-caret-down"></i>
+    </a>
+    <div id="demoAcc" class="w3-bar-block w3-hide w3-padding-large w3-medium">
+      <a href="#" class="w3-bar-item w3-button w3-light-grey"><i class="fa fa-caret-right w3-margin-right"></i>Jordans</a>
+      <a href="#" class="w3-bar-item w3-button">Yeezy</a>
+      <a href="#" class="w3-bar-item w3-button">Nike</a>
+      <a href="#" class="w3-bar-item w3-button">Adidas</a>
+    </div>
+    <a href="#" class="w3-bar-item w3-button">Handbags</a>
+    <a href="#" class="w3-bar-item w3-button">Watches</a>
+  </div>
+  <a href="http://amagsino.web540.net/final/addemail.html" class="w3-bar-item w3-button w3-padding">Subscribe</a> 
+ <a href="http://amagsino.web540.net/final/sendemail.html" class="w3-bar-item w3-button w3-padding">About Us</a> 
+ <a href="http://amagsino.web540.net/final/aboutus.html" class="w3-bar-item w3-button w3-padding">About Us</a> 
+  <a href="http://amagsino.web540.net/final/#footer"  class="w3-bar-item w3-button w3-padding">Login</a>
+</nav>
+
+<!-- Top menu on small screens -->
+<header class="w3-bar w3-top w3-hide-large w3-black w3-xlarge">
+  <div class="w3-bar-item w3-padding-24 w3-wide">Allura Magsino</div>
+  <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-24 w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+</header>
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:250px">
+
+  <!-- Push down content on small screens -->
+  <div class="w3-hide-large" style="margin-top:83px"></div>
+  
+  <!-- Top header -->
+  <header class="w3-container w3-xlarge">
+  <a  href="http://amagsino.web540.net/final" >
+    <p class="w3-left">Sneaker HeadQuarters</p>
+  </a>
+    <p class="w3-right">
+      <i class="fa fa-shopping-cart w3-margin-right"></i>
+      <i class="fa fa-search"></i>
+    </p>
+  </header>
+<script>
+// Accordion 
+function myAccFunc() {
+  var x = document.getElementById("demoAcc");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// Click on the "Jeans" link on page load to open the accordion for demo purposes
+document.getElementById("myBtn").click();
+
+
+// Open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+</script>
+</body>
+
+<body>
+<style>
+table, td {
+  border: 1px solid black;
+}
+th {
+  background-color: navy;
+  color: white;
+  border: 1px solid black;
+}
+</style>
+
+
+<?php
+  $fname = $_POST['F_name'];
+  $lname = $_POST['L_name'];
+  $email = $_POST['email'];
+
+  $dbc = mysqli_connect('localhost', 'amagsino', 'Linda91715', 'amagsino')
+    or die('Error connecting to MySQL server.');
+
+  
+
+  $query = "DELETE FROM final_email WHERE email = '$email'";
+	mysqli_query($dbc, $query)
+    or die('Error querying database.');
+  echo 'Your email: ' . $email, ' has been removed. You will no longer be contacted.';
+
+	$query = "SELECT * FROM final_email";
+		$data = mysqli_query($dbc, $query);
+  	
+
+ echo '<table style= "width:80%">';
+ echo '<tr>';
+ echo '<th>Firstname</th>';
+ echo '<th>Lastname</th>';
+ echo '<th>Email</th>';
+ echo '</tr>';
+
+  while ($row = mysqli_fetch_array($data)) 
+  { 
+   	 echo '<tr>';
+   	 echo '<td >'. $row['first_name'] .'</td>';
+  	 echo '<td >'. $row['last_name'] .'</td>';
+
+      echo '<td class="email_class"> ' . $row['email'] . '</td>';
+
+     echo  '</tr>';
+
+  }
+  echo '</table>';
+
+
+
+
+  mysqli_close($dbc);
+?>
+
+</body>
+</html>

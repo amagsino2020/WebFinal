0da7c4614d9a52132e1a56cdfa1f39444172841c
Allura Magsino <70418023+amagsino2020@users.noreply.github.com>
2020-11-19 18:00:37 -0600

    Add email PHP

---
+++ addemail.php
@@ -0,0 +1,157 @@
+<html>
+<title>Add Email</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.w3-sidebar a {font-family: "Roboto", sans-serif}
+body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
+</style>
+<body class="w3-content" style="max-width:1200px">
+
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
+  <div class="w3-container w3-display-container w3-padding-16">
+    <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
+    <h3 class="w3-wide" style = "text-align: center"><b>SNEAKER HEAD</b></h3>
+  </div>
+  <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
+    <a href="#" class="w3-bar-item w3-button">Handbags</a>
+    <a href="#" class="w3-bar-item w3-button">Streetwear</a>
+    <a onclick="myAccFunc()" href="javascript:void(0)" class="w3-button w3-block w3-white w3-left-align" id="myBtn">
+      Popular Brands <i class="fa fa-caret-down"></i>
+    </a>
+    <div id="demoAcc" class="w3-bar-block w3-hide w3-padding-large w3-medium">
+      <a href="#" class="w3-bar-item w3-button w3-light-grey"><i class="fa fa-caret-right w3-margin-right"></i>Jordans</a>
+      <a href="#" class="w3-bar-item w3-button">Yeezy</a>
+      <a href="#" class="w3-bar-item w3-button">Nike</a>
+      <a href="#" class="w3-bar-item w3-button">Adidas</a>
+    </div>
+    <a href="#" class="w3-bar-item w3-button">Handbags</a>
+    <a href="#" class="w3-bar-item w3-button">Watches</a>
+  </div>
+  <a href="http://amagsino.web540.net/final/addemail.html" class="w3-bar-item w3-button w3-padding">Subscribe</a>
+ <a href="http://amagsino.web540.net/final/sendemail.html" class="w3-bar-item w3-button w3-padding">Send Email </a> 
+ <a href="http://amagsino.web540.net/final/aboutus.html" class="w3-bar-item w3-button w3-padding">About Us</a> 
+  <a href="http://amagsino.web540.net/final/#footer"  class="w3-bar-item w3-button w3-padding">Login</a>
+</nav>
+
+<!-- Top menu on small screens -->
+<header class="w3-bar w3-top w3-hide-large w3-black w3-xlarge">
+  <div class="w3-bar-item w3-padding-24 w3-wide">Allura Magsino</div>
+  <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-24 w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+</header>
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:250px">
+
+  <!-- Push down content on small screens -->
+  <div class="w3-hide-large" style="margin-top:83px"></div>
+  
+  <!-- Top header -->
+  <header class="w3-container w3-xlarge">
+    <p class="w3-left">Sneaker HeadQuarters</p>
+    <p class="w3-right">
+      <i class="fa fa-shopping-cart w3-margin-right"></i>
+      <i class="fa fa-search"></i>
+    </p>
+  </header>
+<script>
+// Accordion 
+function myAccFunc() {
+  var x = document.getElementById("demoAcc");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// Click on the "Jeans" link on page load to open the accordion for demo purposes
+document.getElementById("myBtn").click();
+
+
+// Open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+</script>
+</body>
+
+<body class="w3-content" style="max-width:1200px">
+<style>
+table, td {
+  border: 1px solid black;
+}
+th {
+  background-color: navy;
+  color: white;
+  border: 1px solid black;
+}
+</style>
+
+<?php
+  $dbc = mysqli_connect('localhost', 'amagsino', 'Linda91715', 'amagsino')
+    or die('Error connecting to MySQL server.');
+
+  $first_name = $_POST['firstname'];
+  $last_name = $_POST['lastname'];
+  $email = $_POST['email'];
+
+  $query = "INSERT INTO final_email (first_name, last_name, email)  VALUES ('$first_name', '$last_name', '$email')";
+  mysqli_query($dbc, $query)
+    or die('Error querying database.');
+
+  	echo 'You have been added to Contact us. Click REMOVE if you wish to not be contacted.';
+
+	$query = "SELECT * FROM final_email";
+	$data = mysqli_query($dbc, $query);
+  	
+ 	// Loop through the array of data, formatting it as HTML
+ 	echo '<table style= "width:50%">';
+ 	echo '<tr>';
+ 	echo '<th>Firstname</th>';
+ 	echo '<th>Lastname</th>';
+ 	echo '<th>Email</th>';
+	echo '<th> Action </th>';
+ 	echo '</tr>';
+	
+  while ($row = mysqli_fetch_array($data)) 
+  { 
+   	 echo '<tr>';
+   	 echo '<td >'. $row['first_name'] .'</td>';
+  	 echo '<td >'. $row['last_name'] .'</td>';
+
+      echo '<td class= "email_class" >'. $row['email'] . '</td>';
+ 
+ echo '<td>
+    </form>
+    	<form method = "post" action = "removeemail.php">
+        	<input type = "hidden" name = "L_name" size = "30" value ="'. $row['last_name'].'"/><br />
+            <input type = "hidden" name = "F_name" size = "30" value ="'. $row['first_name'].'"/><br />
+            <input type = "hidden" name = "email" size = "30" value ="'. $row['email'].'"/><br />
+            <input type = "submit" name = "Remove" value = "Remove" />
+     </form>
+     </td>';
+     echo  '</tr>';
+
+  }
+  echo '</table>';
+
+mysqli_close($dbc);
+?>
+</body>
+
+</html>

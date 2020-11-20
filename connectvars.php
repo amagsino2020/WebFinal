fcf42f6e58c497e825a8fe8ee56477e3918d50bf
Allura Magsino <70418023+amagsino2020@users.noreply.github.com>
2020-11-19 18:08:17 -0600

    Profile code

---
+++ connectvars.php
@@ -0,0 +1,8 @@
+  
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'amagsino');
+  define('DB_PASSWORD', 'Linda91715');
+  define('DB_NAME', 'amagsino');
+?>
---
+++ footer.php
@@ -0,0 +1,4 @@
+<hr />
+  <p class="footer">Sneaker Head &copy;2008 Sneaker HeadQuarters, Inc.</p>
+</body>
+</html>

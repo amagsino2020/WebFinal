1311bf347e1c69713c3c0caf039f908b4084e183
Allura Magsino <70418023+amagsino2020@users.noreply.github.com>
2020-11-24 20:48:23 -0600

    Update connectvars.php

--- connectvars.php
+++ connectvars.php
@@ -1,8 +1,8 @@
   
 <?php
   // Define database connection constants
-  define('DB_HOST', 'localhost');
-  define('DB_USER', 'amagsino');
-  define('DB_PASSWORD', 'Linda91715');
-  define('DB_NAME', 'amagsino');
+  define('DB_HOST', 'host');
+  define('DB_USER', 'username');
+  define('DB_PASSWORD', 'password');
+  define('DB_NAME', 'dbname');
 ?>
